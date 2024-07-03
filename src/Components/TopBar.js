2674cf02ee319460b5f261fb538f7a10d645fa45
Rahul <rahul52045@gmail.com>
2024-07-04 01:11:24 +0530

    public room timer and rounds done

--- src/Components/TopBar.js
+++ src/Components/TopBar.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const TopBar = () => {
+const TopBar = ({ timeLeft, round, totalRounds }) => {
   return (
     <div className="w-full bg-blue-500 text-white p-4 flex justify-between items-center">
-      <span>Round 2 of 3</span>
-      <span>Timer: 60s</span>
+      <span>Round {round} of {totalRounds}</span>
+      <span>Timer: {timeLeft}s</span>
     </div>
   );
 };
